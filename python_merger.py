c9a7dd216bb85c6a3b739e2b06dc2ef9f2da720d
Victor Feller <VictorFeller@users.noreply.github.com>
2022-01-03 21:08:04 +0100

    Add files via upload

---
+++ python_merger.py
@@ -0,0 +1,18 @@
+import os
+from PyPDF2 import PdfFileMerger
+
+#Current dir_path where the script is executed
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
+
+#Add pdf files in an array[]
+pdfs = [file for file in os.listdir(dir_path) if file.endswith('pdf')]
+
+merger = PdfFileMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("merge.pdf")
+merger.close()
+#os.system("pause")
